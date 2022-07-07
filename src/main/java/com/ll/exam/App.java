0ddacfd90d5f642a4dc06555542183a506cd2b7d
jihun-24k <gi020477@gmail.com>
2022-07-07 14:57:10 +0900

    Feat: 등록 기능 구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -3,9 +3,22 @@ package com.ll.exam;
 import java.util.Scanner;
 
 public class App {
+
+    public void add(int idx){
+        Scanner sc1 = new Scanner(System.in);
+
+        System.out.printf("명언 : ");
+        sc1.nextLine();
+        System.out.printf("작가 : ");
+        sc1.nextLine();
+
+        System.out.println(idx+"번 글이 등록되었습니다.");
+    }
+
     public void run() {
         System.out.println("-- 명언 SSG --");
         Scanner sc = new Scanner(System.in);
+        int idx = 1;
 
         outer:
         while (true){
@@ -16,6 +29,10 @@ public class App {
             switch (cmd){
                 case "종료":
                     break outer;
+                case "등록":
+                    add(idx);
+                    idx++;
+
             }
         }
         sc.close();
