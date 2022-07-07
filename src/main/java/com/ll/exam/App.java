ada56ef423cca50a1b373b004cb88be0222d25cb
jihun-24k <gi020477@gmail.com>
2022-07-07 14:45:25 +0900

    Feat: 명령 프롬프트 구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,5 +1,23 @@
 package com.ll.exam;
 
-public class App {
+import java.util.Scanner;
 
+public class App {
+    public void run() {
+        System.out.println("-- 명언 SSG --");
+        Scanner sc = new Scanner(System.in);
+
+        outer:
+        while (true){
+            System.out.printf("명령) ");
+            // trim()은 띄어쓰기를 삭제
+            String cmd = sc.nextLine().trim();
+
+            switch (cmd){
+                case "종료":
+                    break outer;
+            }
+        }
+        sc.close();
+    }
 }
