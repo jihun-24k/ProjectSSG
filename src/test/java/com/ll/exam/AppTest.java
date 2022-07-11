86634146ad62d7bba835665ec0f2a7d3fe20c7b2
jihun-24k <gi020477@gmail.com>
2022-07-11 15:35:52 +0900

    Feat: 미리 입력된 값을 받아들일 수 있는 객체

---
+++ src/test/java/com/ll/exam/AppTest.java
@@ -0,0 +1,32 @@
+package com.ll.exam;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    public void ScannerTest(){
+        String input = """
+                등록
+                명언1
+                작가1
+                """.stripIndent();
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        Scanner sc = new Scanner(in);
+
+        String cmd = sc.nextLine().trim();
+        String content = sc.nextLine().trim();
+        String author = sc.nextLine().trim();
+
+        assertEquals("등록", cmd);
+        assertEquals("명언1", content);
+        assertEquals("작가1", author);
+    }
+
+}
