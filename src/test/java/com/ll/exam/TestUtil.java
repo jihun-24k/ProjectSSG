7622bf7a2e700b64ad8a2084e5da6936c9dfa64d
jihun-24k <gi020477@gmail.com>
2022-07-15 17:28:29 +0900

    Feat: JSON을 Map으로 저장

--- src/test/java/com/ll/exam/TestUtil.java
+++ src/test/java/com/ll/exam/TestUtil.java
@@ -2,6 +2,8 @@ package com.ll.exam;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestUtil {
@@ -27,5 +29,21 @@ public class TestUtil {
         String rs = Util.readFromFile("test_data/1.json");
 
         assertEquals(rs, strInstance);
+    }
+
+    @Test
+    void JsonToMapTest(){
+        Util.mkdir("test_data");
+        Post post = new Post(1, "난 아직 배고프다","히딩크");
+        String strInstance = post.toJson();
+
+        Util.saveToFile("test_data/1.json",strInstance);
+
+        String json = Util.readFromFile("test_data/1.json");
+        Map<String, Object> rs = Util.jsonToMap(json);
+
+        assertEquals(rs.get("id"), 1);
+        assertEquals(rs.get("contents"), "난 아직 배고프다");
+        assertEquals(rs.get("author"), "히딩크");
     }
 }
