30f3803db9e7c818edcdc0f204fcad80e4ea3779
jihun-24k <gi020477@gmail.com>
2022-07-17 20:48:02 +0900

    Feat: 특정 파일에 JSON 파일을 모두 객체화하기

--- src/test/java/com/ll/exam/TestUtil.java
+++ src/test/java/com/ll/exam/TestUtil.java
@@ -3,7 +3,10 @@ package com.ll.exam;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -76,5 +79,44 @@ public class TestUtil {
         int rs = Util.readNumberFromFile("test_data/last_id.txt", 0);
 
         assertEquals(100, rs);
+    }
+
+    @Test
+    void loadAllFilesInDirectory() {
+        Util.saveNumberToFile("test_data/1.txt", 1);
+        Util.saveNumberToFile("test_data/2.txt", 1);
+        Util.saveNumberToFile("test_data/3.txt", 1);
+
+        List<String> fileNames = Util.getFileNamesFromDir("test_data");
+
+        assertEquals(fileNames.get(2), "1.txt");
+        assertEquals(fileNames.get(1), "2.txt");
+        assertEquals(fileNames.get(0), "3.txt");
+    }
+    @Test
+    void 특정_폴더에_들어있는_JSON_파일들만_모아서_객체화_시킨_후_리스트에_담기() {
+        Post wiseSaying1 = new Post(1, "내 사전에 불가능은 없다.", "나폴레옹");
+        Post wiseSaying2 = new Post(2, "나의 죽음을 적들에게 알리지 마라.", "이순신");
+
+        Util.saveToFile("test_data/1.json", wiseSaying1.toJson());
+        Util.saveToFile("test_data/2.json", wiseSaying2.toJson());
+        Util.saveNumberToFile("test_data/last_id.txt", 2);
+
+        List<String> fileNames =
+                Util.getFileNamesFromDir("test_data")
+                        .stream() // 리스트를 스트림으로 변환
+                        .filter(fileName -> fileName.endsWith(".json")) // 파일명이 .json 으로 끝나지 않는 것들은 모두 버림
+                        .collect(Collectors.toList()); // 스트림을 다시 리스트로 변환
+
+        List<Post> wiseSayings = new ArrayList<>();
+        for (String fileName : fileNames) {
+            String rs = Util.readFromFile("test_data/" + fileName);
+            Map<String, Object> map = Util.jsonToMap(rs);
+            Post wiseSaying = new Post(map);
+            wiseSayings.add(wiseSaying);
+        }
+
+        assertEquals(wiseSaying1, wiseSayings.get(0));
+        assertEquals(wiseSaying2, wiseSayings.get(1));
     }
 }
